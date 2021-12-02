3c9e394f263f69cf24394c33023e6d06dda5d384
Michal Broniewicz <mbroniewicz@outlook.com>
2021-12-02 11:41:59 +0100

    02 part 2 solution

--- 02/solution.rb
+++ 02/solution.rb
@@ -11,14 +11,38 @@ depth = 0
 
 input.each do |line|
   action, delta = line.split
+  delta_i = delta.to_i
   case action
   when 'forward'
-    horizontal += delta.to_i
+    horizontal += delta_i
   when 'up'
-    depth -= delta.to_i
+    depth -= delta_i
   else
-    depth += delta.to_i
+    depth += delta_i
   end
 end
 
+# 2187380
+puts horizontal * depth
+
+# part 2
+horizontal = 0
+depth = 0
+aim = 0
+
+input.each do |line|
+  action, delta = line.split
+  delta_i = delta.to_i
+  case action
+  when 'forward'
+    horizontal += delta_i
+    depth += (aim * delta_i)
+  when 'up'
+    aim -= delta_i
+  else
+    aim += delta_i
+  end
+end
+
+# 2086357770
 puts horizontal * depth
