e21f2d7c97e790b7db3947f955510f0e62744abb
Michal Broniewicz <mbroniewicz@outlook.com>
2021-12-02 12:22:17 +0100

    02 part1 and part2 refactor

--- 02/solution.rb
+++ 02/solution.rb
@@ -1,48 +1,36 @@
 # input read
 input = Array.new
+tuple = Struct.new(:action, :delta)
 
 File.open('02.txt').each do |line|
-  input.append(line)
+  action, delta = line.split
+  input.append(tuple.new(action, delta.to_i))
+end
+
+def move(input, actions)
+  horizontal = 0
+  depth = 0
+  aim = 0
+  input.each do |tuple|
+    horizontal, depth, aim = actions[tuple.action].call(horizontal, depth, aim, tuple.delta)
+  end
+  horizontal * depth
 end
 
 # part1
-horizontal = 0
-depth = 0
-
-input.each do |line|
-  action, delta = line.split
-  delta_i = delta.to_i
-  case action
-  when 'forward'
-    horizontal += delta_i
-  when 'up'
-    depth -= delta_i
-  else
-    depth += delta_i
-  end
-end
+actions_one = Hash.new
+actions_one['forward'] = -> (h, d, a, delta) { h += delta; return h, d, a }
+actions_one['up'] = -> (h, d, a, delta) { d -= delta; return h, d, a }
+actions_one['down'] = -> (h, d, a, delta) { d += delta; return h, d, a }
 
 # 2187380
-puts horizontal * depth
+puts move(input, actions_one)
 
 # part 2
-horizontal = 0
-depth = 0
-aim = 0
-
-input.each do |line|
-  action, delta = line.split
-  delta_i = delta.to_i
-  case action
-  when 'forward'
-    horizontal += delta_i
-    depth += (aim * delta_i)
-  when 'up'
-    aim -= delta_i
-  else
-    aim += delta_i
-  end
-end
+actions_two = Hash.new
+actions_two['forward'] = -> (h, d, a, delta) { h += delta; d += (a * delta); return h, d, a }
+actions_two['up'] = -> (h, d, a, delta) { a -= delta; return h, d, a }
+actions_two['down'] = -> (h, d, a, delta) { a += delta; return h, d, a }
 
 # 2086357770
-puts horizontal * depth
+puts move(input, actions_two)
